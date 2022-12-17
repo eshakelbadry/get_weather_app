50ffbb48e07e7c316adadab024a3712905c41ead
eshakelbadry <eshak.elbadry96@gmail.com>
2022-12-17 23:14:37 +0200

    irst commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -22,95 +22,147 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     weatherData = Provider.of<WeatherProvider>(context).weatherData;
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return SearchPage(
-                    updateUi: updateUi,
-                  );
-                }));
-              },
-              icon: Icon(Icons.search))
-        ],
-        title: Text('Weather App'),
-      ),
-      body: weatherData == null
-          ? Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(
-                    'there is no weather 😔 start searchig',
-                    style: TextStyle(
-                      fontSize: 25,
-                    ),
-                  ),
-                  Text(
-                    'searching now 🔍',
-                    style: TextStyle(
-                      fontSize: 25,
-                    ),
-                  )
-                ],
-              ),
-            )
-          : Container(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: [
-                weatherData!.getThemeColor(),
-                weatherData!.getThemeColor()[300]!,
-                weatherData!.getThemeColor()[100]!
-              ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Spacer(flex: 3),
-                  Container(
-                    child: Text(
-                      Provider.of<WeatherProvider>(context).cityName!,
-                      style:
-                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  Container(
-                    child: Text(
-                      'updated at : ${weatherData!.date.hour.toString()}:${weatherData!.date.minute.toString()}',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                  const Spacer(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: const Color.fromARGB(255, 4, 88, 39),
+          // actions: [
+          //   IconButton(
+          //       onPressed: () {
+          //         Navigator.push(context, MaterialPageRoute(builder: (context) {
+          //           return SearchPage(
+          //             updateUi: updateUi,
+          //           );
+          //         }));
+          //       },
+          //       icon: Icon(Icons.search))
+          // ],
+          // title: Text('Weather Status'),
+        ),
+        body: weatherData == null
+            ? Container(
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                  Color.fromARGB(255, 4, 88, 39),
+                  Color.fromARGB(255, 31, 131, 73),
+                  Color.fromARGB(255, 111, 169, 135),
+                  Color.fromARGB(255, 142, 186, 160),
+                ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+                child: Center(
+                  child: Column(
+                    // mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      Image.asset(weatherData!.getImage()),
-                      Text(
-                        '${weatherData!.temp.toInt()}',
-                        style: TextStyle(
-                            fontSize: 30, fontWeight: FontWeight.bold),
+                      Container(
+                        height: 80,
+                        width: 250,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            'Weather Status',
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
                       ),
-                      Column(
+                      const SizedBox(height: 40),
+                      const Text(
+                        'There is no weather 😔',
+                        style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Text('maxTemp :${weatherData!.maxTemp.toInt()}'),
-                          Text('minTemp : ${weatherData!.minTemp.toInt()}'),
+                          const Text(
+                            'Start searching now',
+                            style: TextStyle(
+                              fontSize: 25,
+                              color: Colors.white,
+                            ),
+                          ),
+                          IconButton(
+                              onPressed: () {
+                                Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) {
+                                  return SearchPage(
+                                    updateUi: updateUi,
+                                  );
+                                }));
+                              },
+                              icon: const Icon(
+                                Icons.search,
+                                size: 35,
+                              )),
                         ],
                       )
                     ],
                   ),
-                  const Spacer(),
-                  Container(
-                    child: Text(
-                      weatherData?.weatherStatusName ?? '',
-                      style:
-                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+              )
+            : Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                  weatherData!.getThemeColor(),
+                  weatherData!.getThemeColor()[300]!,
+                  weatherData!.getThemeColor()[100]!
+                ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Spacer(flex: 3),
+                    Container(
+                      child: Text(
+                        Provider.of<WeatherProvider>(context).cityName!,
+                        style: const TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
                     ),
-                  ),
-                  const Spacer(flex: 5),
-                ],
+                    Container(
+                      child: Text(
+                        'updated at : ${weatherData!.date.hour.toString()}:${weatherData!.date.minute.toString()}',
+                        style: const TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    const Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Image.asset(weatherData!.getImage()),
+                        Text(
+                          '${weatherData!.temp.toInt()}',
+                          style: const TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold),
+                        ),
+                        Column(
+                          children: [
+                            Text('maxTemp :${weatherData!.maxTemp.toInt()}'),
+                            Text('minTemp : ${weatherData!.minTemp.toInt()}'),
+                          ],
+                        )
+                      ],
+                    ),
+                    const Spacer(),
+                    Container(
+                      child: Text(
+                        weatherData?.weatherStatusName ?? '',
+                        style: const TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const Spacer(flex: 5),
+                  ],
+                ),
               ),
-            ),
+      ),
     );
   }
 }
